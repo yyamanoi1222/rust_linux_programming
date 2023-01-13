c34ac9b8c9a6b34796805b0cd603867e8f933e9b
Yuu Yamanoi <yuu.yamanoi1222@gmail.com>
2023-01-13 23:16:26 +0900

    wip

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buf = String::new();
+    let args: Vec<String> = env::args().collect();
+
+    // parse some options
+
+    if args.len() > 1 {
+        println!("{}", args[1]);
+
+        // if arg is file
+        // cat file
+    } else {
+        io::stdin().read_line(&mut buf);
+        println!("{}", buf);
+    }
 }
